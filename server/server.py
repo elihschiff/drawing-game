6ddcce533457372bb91bcfe3dd6524a524b3ab3e
Eli Schiff <eli@elischiff.org>
2021-04-21 13:04:44 -0400

    Add auto reconnect for clients as well as game_data with points to server

--- server/server.py
+++ server/server.py
@@ -1,24 +1,36 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import json
 
 clients = []
+game_data = {
+    'points': {}
+}
 class SimpleChat(WebSocket):
-
     def handleMessage(self):
-       for client in clients:
-          if client != self:
-             client.sendMessage(self.data)
+        print(self.data)
+        if not self.data:
+            return
+        data = json.loads(self.data)
+
+        if 'type' in data and data['type'] == 'connected':
+            if data['id'] not in game_data['points']:
+                game_data['points'][data['id']] = 0
+        else:
+            for client in clients:
+                if client != self:
+                    client.sendMessage(self.data)
 
     def handleConnected(self):
-       print(self.address, 'connected')
-       # for client in clients:
-       #    client.sendMessage(self.address[0] + u' - connected')
-       clients.append(self)
+        print(self.address, 'connected')
+        # for client in clients:
+        #    client.sendMessage(self.address[0] + u' - connected')
+        clients.append(self)
 
     def handleClose(self):
-       clients.remove(self)
-       print(self.address, 'closed')
-       # for client in clients:
-       #    client.sendMessage(self.address[0] + u' - disconnected')
+        clients.remove(self)
+        print(self.address, 'closed')
+        # for client in clients:
+        #    client.sendMessage(self.address[0] + u' - disconnected')
 
 server = SimpleWebSocketServer('', 8000, SimpleChat)
 server.serveforever()
