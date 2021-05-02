4f7dcab42a9de85872f7e4b8267c9549c60b53f8
Eli Schiff <eli@elischiff.org>
2021-05-02 14:39:10 -0400

    Fix issues in server

--- server/server.py
+++ server/server.py
@@ -6,37 +6,34 @@ clients = []
 game = {
     "word": None,
     "drawer": None,
-    'drawer_id': -1,
+    "drawer_id": -1,
     "points": {},
 }
 
 words = []
-with open('words.txt', 'r') as f:
+with open("words.txt", "r") as f:
     words = f.readlines()
     words = [word.lower().strip() for word in words]
 
 
 def getwords():
-    game['drawer_id']+=1
-    game['drawer_id']%=len(clients)
-    game['drawer'] = clients[game['drawer_id']]
-    msg = {
-        'type': 'getwords',
-        'drawer': False,
-        'words': random.sample(words, 5)
-    }
+    game["drawer_id"] += 1
+    game["drawer_id"] %= len(clients)
+    game["drawer"] = clients[game["drawer_id"]]
+    msg = {"type": "getwords", "drawer": False, "words": random.sample(words, 5)}
 
     # TODO allow user to pick word
-    game['word'] = msg['words'][0]
+    game["word"] = msg["words"][0]
 
     for client in clients:
-        if client != game['drawer']:
+        if client != game["drawer"]:
             client.sendMessage(json.dumps(msg))
 
-    msg['drawer'] = True
-    game['drawer'].sendMessage(json.dumps(msg))
+    msg["drawer"] = True
+    game["drawer"].sendMessage(json.dumps(msg))
 
     print("getwords ran")
+
 
 class SimpleChat(WebSocket):
     def handleMessage(self):
@@ -45,30 +42,33 @@ class SimpleChat(WebSocket):
             return
         data = json.loads(self.data)
 
-        if game['drawer'] == None:
+        if game["drawer"] == None:
             getwords()
 
-        if data['type'] == "msg" and data['msgtype'] == 'guess' and data['msg'] == game['word']:
-            self.sendMessage(json.dumps({
-                'type':'system',
-                'msg':'correct'
-            }))
+        if (
+            data["type"] == "msg"
+            and data["msgtype"] == "guess"
+            and data["msg"] == game["word"]
+        ):
+            self.sendMessage(json.dumps({"type": "system", "msg": "correct"}))
             for client in clients:
                 if client != self:
-                    client.sendMessage(json.dumps({
-                        'type':'system',
-                        'msg':'guessed',
-                        'real_word':game['word'],
-                    }))
+                    client.sendMessage(
+                        json.dumps(
+                            {
+                                "type": "system",
+                                "msg": "guessed",
+                                "real_word": game["word"],
+                            }
+                        )
+                    )
             getwords()
             return
-
 
         if data["type"] == "connected":
             if data["id"] not in game["points"]:
                 game["points"][data["id"]] = 0
             return
-
 
         for client in clients:
             if client != self:
@@ -80,15 +80,14 @@ class SimpleChat(WebSocket):
         #    client.sendMessage(self.address[0] + u' - connected')
         clients.append(self)
 
-
-        if game['drawer'] == None:
+        if game["drawer"] == None:
             getwords()
         else:
-            self.sendMessage(json.dumps({
-                'type': 'getwords',
-                'drawer': False,
-                'words': [game['word']]
-            }))
+            self.sendMessage(
+                json.dumps(
+                    {"type": "getwords", "drawer": False, "words": [game["word"]]}
+                )
+            )
 
     def handleClose(self):
         clients.remove(self)
